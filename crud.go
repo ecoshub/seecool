54e9df0046ec48e9ef6636725f7488f2d9e04e02
eco9999 <emrecanocak@windowslive.com>
2020-03-19 22:34:03 +0300

    new general error imp.

--- crud.go
+++ crud.go
@@ -2,23 +2,15 @@ package sqljson
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"jin"
 )
 
-var (
-	jin_err     string = "Jin package"
-	db_err      string = "Database"
-	no_data_err string = "No entry found."
-	conv_error  string = "JSON conversion error"
-)
-
-func errFunc(str, err string) string {
-	return str + " error:" + err
-}
+var entryNotExiest string = "Entry not exist."
 
 // {"first_name":"Jim","last_name":"Carter","email":"jimmy@gmail.com","gender":"Male","ip_address":"192.168.1.108","country":"Iraq","birth_date":"1988-01-17"}
-func Create(db *sql.DB, json []byte, table string) []byte {
+func Create(db *sql.DB, json []byte, table string) error {
 	query := `insert into ` + table + ` (`
 	keys := ``
 	values := ``
@@ -28,20 +20,20 @@ func Create(db *sql.DB, json []byte, table string) []byte {
 		return true
 	})
 	if err != nil {
-		return jin.MakeJsonString([]string{"event", "error_text"}, []string{"error", errFunc(jin_err, err.Error())})
+		return err
 	}
 	keys = keys[:len(keys)-1]
 	values = values[:len(values)-1]
 	query += keys + `) values (` + values + `)`
 	_, err = db.Query(query)
 	if err != nil {
-		return jin.MakeJsonString([]string{"event", "error_text"}, []string{"error", errFunc(db_err, err.Error())})
+		return err
 	}
-	return jin.MakeJsonString([]string{"event"}, []string{"OK"})
+	return nil
 }
 
 // {"key":"value","key2":"value2"} with and condition
-func Read(db *sql.DB, json []byte, table string) []byte {
+func Read(db *sql.DB, json []byte, table string) ([]byte, error) {
 	query := `select * from ` + table + ` where `
 	err := jin.IterateKeyValue(json, func(key, value []byte) bool {
 		query += string(key) + ` = '` + string(value) + `'`
@@ -49,82 +41,84 @@ func Read(db *sql.DB, json []byte, table string) []byte {
 		return true
 	})
 	if err != nil {
-		return jin.MakeJsonString([]string{"event", "error_text"}, []string{"error", errFunc(jin_err, err.Error())})
+		return nil, err
 	}
 	query = query[:len(query)-5]
 	rows, err := db.Query(query)
 	if err != nil {
-		return jin.MakeJsonString([]string{"event", "error_text"}, []string{"error", errFunc(db_err, err.Error())})
+		return nil, err
 	}
 	json, err = RowsToJsonByte(rows)
 	if err != nil {
-		return jin.MakeJsonString([]string{"event", "error_text"}, []string{"error", errFunc(conv_error, err.Error())})
+		return nil, err
 	}
 	if string(json) == `[]` {
-		return jin.MakeJsonString([]string{"event", "warning_text"}, []string{"warning", no_data_err})
+		return nil, errors.New(entryNotExiest)
 	}
-	return json
+	fmt.Println(json)
+	return json, nil
 }
 
 // {"unique_key":"last_name","value":"john","key":"email","new_value":"johnnyy@windowslive.com"}
-func Update(db *sql.DB, json []byte, table string) bool {
+func Update(db *sql.DB, json []byte, table string) error {
 	key, err := jin.GetString(json, "key")
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 	unique_key, err := jin.GetString(json, "unique_key")
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 	value, err := jin.GetString(json, "value")
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 	newValue, err := jin.GetString(json, "new_value")
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
-	if !existCore(db, table, unique_key, value) {
-		fmt.Println("entry not exist.")
-		return false
+	exist, err := existCore(db, table, unique_key, value)
+	if err != nil {
+		return err
+	}
+	if !exist {
+		return errors.New(entryNotExiest)
 	}
 	query := `update ` + table + ` set ` + key + ` = '` + newValue + `' where ` + unique_key + ` = '` + value + `'`
 	_, err = db.Query(query)
 	if err != nil {
-		fmt.Println(err)
-		return false
+		return err
 	}
-	fmt.Println("entry updated.")
-	return true
+	return nil
 }
 
 // {"unique_key":"email","value":"johnnyy@windowslive.com"}
-func Delete(db *sql.DB, json []byte, table string) bool {
+func Delete(db *sql.DB, json []byte, table string) error {
 	key, err := jin.GetString(json, "key")
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 	value, err := jin.GetString(json, "value")
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
-	if !existCore(db, table, key, value) {
-		fmt.Println("entry not exist.")
-		return false
+	exist, err := existCore(db, table, key, value)
+	if err != nil {
+		return err
+	}
+	if !exist {
+		return errors.New(entryNotExiest)
 	}
 	query := `delete from ` + table + ` where ` + key + ` = '` + value + `'`
-	fmt.Println(query)
 	_, err = db.Query(query)
 	if err != nil {
-		fmt.Println(err)
-		return false
+		return err
 	}
-	fmt.Println("entry deleted.")
-	return true
+	return nil
 }
 
 // {"key":"value","key2":"value2"} with regex any condition
-func Find(db *sql.DB, json []byte, table string) []byte {
+func Find(db *sql.DB, json []byte, table string) ([]byte, error) {
 	query := `select * from ` + table + ` where `
 	in := false
 	err := jin.IterateKeyValue(json, func(key, value []byte) bool {
@@ -134,31 +128,27 @@ func Find(db *sql.DB, json []byte, table string) []byte {
 		return true
 	})
 	if err != nil {
-		fmt.Println(err)
-		return nil
+		return nil, err
 	}
 	if in {
 		// delete last 'and' statement.
 		query = query[:len(query)-5]
 	} else {
-		fmt.Println("no entry.")
-		return nil
+		return nil, errors.New(entryNotExiest)
 	}
 	rows, err := db.Query(query)
 	if err != nil {
-		fmt.Println(err)
-		return nil
+		return nil, err
 	}
 	json, err = RowsToJsonByte(rows)
 	if err != nil {
-		fmt.Println(err)
-		return nil
+		return nil, err
 	}
-	return json
+	return json, nil
 }
 
 // {"key":"value","key2":"value2"} with and condition
-func Count(db *sql.DB, json []byte, table string) int {
+func Count(db *sql.DB, json []byte, table string) (int, error) {
 	query := `select count(*) from ` + table + ` where `
 	in := false
 	err := jin.IterateKeyValue(json, func(key, value []byte) bool {
@@ -168,56 +158,46 @@ func Count(db *sql.DB, json []byte, table string) int {
 		return true
 	})
 	if err != nil {
-		fmt.Println(err)
-		return 0
+		return 0, err
 	}
 	if in {
 		// delete last 'and' statement.
 		query = query[:len(query)-5]
 	} else {
 		fmt.Println("no entry.")
-		return 0
+		return 0, errors.New(entryNotExiest)
 	}
 	rows, err := db.Query(query)
 	if err != nil {
-		fmt.Println(err)
-		return 0
+		return 0, err
 	}
 	json, err = RowsToJsonByte(rows)
 	if err != nil {
-		fmt.Println(err)
-		return 0
+		return 0, err
 	}
 	val, err := jin.GetInt(json, "0", "count")
 	if err != nil {
-		fmt.Println(err)
-		return 0
+		return 0, err
 	}
-	if val > 0 {
-		return val
-	}
-	return 0
+	return val, nil
 }
 
-func existCore(db *sql.DB, table, unique_key, value string) bool {
+func existCore(db *sql.DB, table, unique_key, value string) (bool, error) {
 	query := `select count(*) from ` + table + ` where ` + unique_key + ` = '` + value + `'`
 	rows, err := db.Query(query)
 	if err != nil {
-		fmt.Println(err)
-		return false
+		return false, err
 	}
 	json, err := RowsToJsonByte(rows)
 	if err != nil {
-		fmt.Println(err)
-		return false
+		return false, err
 	}
 	val, err := jin.GetInt(json, "0", "count")
 	if err != nil {
-		fmt.Println(err)
-		return false
+		return false, err
 	}
 	if val > 0 {
-		return true
+		return true, nil
 	}
-	return false
+	return false, nil
 }
