c1f2bd1290d9d9329fe2a88bfc2f09f18787f2d5
eco9999 <emrecanocak@windowslive.com>
2020-03-25 16:30:15 +0300

    error_code imp.

--- error.go
+++ error.go
@@ -3,6 +3,6 @@ package seecool
 import "errors"
 
 var (
-	errMalformedQuery error = errors.New("Malformed Query.")
-	errMissingKVQuery error = errors.New("Missing key and value fro INSERT query.")
+	errMalformedQuery error = errors.New("Malformed Query. error_code:00")
+	errMissingKVQuery error = errors.New("Missing key and value fro INSERT query. error_code:01")
 )
